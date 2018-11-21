8b5a7cff58e955b56bf6998be63b3c82d50144d6
MatheusAquatico <43749592+MatheusAquatico@users.noreply.github.com>
2018-11-21 18:23:56 -0200

    Segunda submissão com a parte de Raissa junto

---
+++ SatSolverVersão0.1.js
@@ -0,0 +1,145 @@
+//Começa aqui o projeto
+/*
+to do:
+read variables;
+do solve;
+*/
+
+exports.solve = function(fileName) {
+  let formula = readFormula(fileName)
+  let result = doSolve(formula.clauses, formula.variables)
+  return result // two fields: isSat and satisfyingAssignment
+}
+
+function generateAssigments(variables) {
+    let arrayOfAssigments = new Array(variables * variables)(variables);
+    for (let x = 0; x < variables * variables; x++) {
+      for (let y = 0; y < variables; y++) {
+        arrayOfAssigments[x][y] = 0;
+      }
+    }// Aqui cria um array bidimensional com todos vaalores falsos (0);
+    for (let a = 0; x < variables * variables; x++) {
+      var phrase1 = phrase2;
+      for (let b = 0; b < variables; b++) {
+        var phrase2 += arrayOfAssigments[a][b];
+      }
+      while (phrase2.equals(phrase1)) {
+        let add = 0;
+        arrayOfAssigments[a][add] = 1;
+        for (let c = 0; b < variables; c++) {
+          var phrase2 += arrayOfAssigments[a][c];
+        }
+      }
+    }//Aqui cria as diferentes soluções comparando elas entre si
+  
+  }
+  function nextAssignment(currentAssignment, arrayOfAssigments) {
+    //Aqui gera uma nova tentativa se a outra não satisfez o problema
+    for (let m = 0; m < arrayOfAssigments.length; m++) {
+      for (let n = 0; n < arrayOfAssigments[m.length; n++]) {
+        if (!array.equals(arrayOfAssigments[m][n])) {
+          var newAssignment = arrayOfAssigments[m][n];
+          break;
+        } else {
+          continue;
+        }
+      }
+    }
+    return newAssignment
+  }
+
+function doSolve(clauses, assignment) {
+  let isSat = false
+  while ((!isSat) && /* must check whether this is the last assignment or not*/) {
+    // does this assignment satisfy the formula? If so, make isSat true. 
+
+    // if not, get the next assignment and try again. 
+    assignment = nextAssignment(assignment)
+  }
+  let result = {'isSat': isSat, satisfyingAssignment: null}
+  if (isSat) {
+    result.satisfyingAssignment = assignment
+  }
+  return result
+}
+  
+ // ---------- Ler Arquivo -------------- 
+ function readFormula(fileName) {
+  
+    var fs = require('fs');
+    let file = fs.readFileSync(Filename).toString();
+    let text = file.split('\n');
+    let clauses = readClauses(text)
+    let variables = readVariables(clauses)
+  
+    let specOk = checkProblemSpecification(text, clauses, variables);
+    let result = { 'clauses': [], 'variables': [] };
+    if (specOk) {
+      result.clauses = clauses;
+      result.variables = variables;
+    }
+    return result;
+  }
+
+  // ---------- Ler Cláusulas -------------
+  function readclauses(text) {
+    let strgClauses = "";
+    
+    for (let u = 0; u<text.length; u++ ){
+        if (text[u].charAt(0) !== 'c' && text[u].charAt(0) !== 'p') {
+            strgClauses = strgClauses +' '+text[u];
+        }
+    let clauses = [];
+    let clausesAux = [];
+    
+    if (strgClauses.length > 0 && strgClauses[clauses.length - 1] == ' 0' ) {
+        clausesAux = strgClauses.pop();
+    
+        clauses.push(clausesAux);
+        clausesAux = [];
+    }
+    }
+    
+    return clauses;
+    
+  let specok = checkProblemSpecification(text, clauses, variables);
+
+  function checkProblemSpecification(text, clauses, variables) {
+    //Aqui só checa se os parâmetros batem. Se o texto for vazio ou não bater os parâmetros, retorna false.
+    var clauseLength = clauses.length;
+    var checklist = [];
+    if (text.length == 0) {
+      return false;
+    } else {
+      for (let i = 0, espaco = 0, frase = 0; i < text.length; i++) {
+        espaco = test[i];
+        if (espaco == 0) {
+          frase++;
+        } else {
+          for (let a = 0; a <= checklist.length; a++) {
+            if (espaco != checklist.length[a]) {
+              continue;
+            } else if (espaco == checklist.length[a]) {
+              break;
+            } else {
+              checklist.push(espaco);
+            }
+          }
+        }
+  
+      }
+      if (frase == clauses && checklist.lengh == variables) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  let result = { 'clauses': [], 'variables': [] }
+  if (specOk) {
+    result.clauses = clauses
+    result.variables = variables
+  }
+  return result
+}
